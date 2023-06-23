ea3618d005200def6e92bb2fc1c0db4a1ab1b47d
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2023-06-23 16:12:27 +0100

    fixes

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,6 +1,6 @@
 import { PER_PAGE } from 'constants/pagination';
 import http from 'plugins/axios';
-
+import { isGcAdmin } from 'utils/getUserRole';
 class TransactionService {
   async getTransactionPerOrganization(params) {
     try {
@@ -21,8 +21,12 @@ class TransactionService {
     }
   }
   async getAllTransactions(params) {
+    let url = '/api/requests/backoffice/transfer-requests/all';
+    if (isGcAdmin()) {
+      url = '/api/gcadmin/transferRequest';
+    }
     try {
-      const { data } = await http.get('/api/requests/backoffice/transfer-requests/all', {
+      const { data } = await http.get(url, {
         params: { ...params, perPage: PER_PAGE }
       });
       return data;
