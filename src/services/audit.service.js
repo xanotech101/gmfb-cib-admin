299832e1732173c22a21fd0853d18ff450937bed
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2023-06-23 08:51:48 +0100

    fixes

--- src/services/audit.service.js
+++ src/services/audit.service.js
@@ -1,5 +1,6 @@
 import http from 'plugins/axios';
 import { PER_PAGE } from 'constants/pagination';
+import { isGcAdmin } from 'utils/getUserRole';
 class AuditService {
   async getOrganizationAuditTrails(params) {
     try {
@@ -13,8 +14,12 @@ class AuditService {
   }
 
   async getAllAuditTrails(params) {
+    let url = '/api/audit_trails/all';
+    if (isGcAdmin()) {
+      url = '/api/gcadmin/audit-trails';
+    }
     try {
-      const { data } = await http.get('/api/audit_trails/all', {
+      const { data } = await http.get(url, {
         params: { ...params, perPage: PER_PAGE }
       });
       return data;
